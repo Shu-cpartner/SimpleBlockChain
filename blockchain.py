045c5126c3ebbca12cc34b85f8be77533b73e70c
Shuji <zaha@cprtnr.com>
2022-07-09 16:40:31 +0900

    Implement is_valid request if the Blockchain is valid

--- blockchain.py
+++ blockchain.py
@@ -49,7 +49,7 @@ class Blockchain:
         block_index = 1
         while block_index < len(chain):
             block = chain[block_index]
-            if block['previou_hash'] != self.hash(previous_block):
+            if block['previous_hash'] != self.hash(previous_block):
                 return False
             previous_proof = previous_block['proof']
             proof = block['proof']
@@ -96,6 +96,16 @@ def get_chain():
         'chain': blockchain.chain,
         'length': len(blockchain.chain)
     }
+    return jsonify(response), 200
+
+# Checking if the Blockchain is valid
+@app.route('/is_valid', methods = ['GET'])
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid:
+        response = {'message': 'All good. The Blockchain is valid.'}
+    else:
+        response = {'message': 'Houston, we have a problem. The Blockchain is not valid.'}
     return jsonify(response), 200
 
 # Running the app
